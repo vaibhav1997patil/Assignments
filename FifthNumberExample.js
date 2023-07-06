0c4e6321a2e45350d3e5b6ae0df824f77b55a8b0
vaibhav1997patil <vnpatil199@gmail.com>
2023-07-06 11:51:07 +0530

    fifth commit

---
+++ FifthNumberExample.js
@@ -0,0 +1,7 @@
+function average(a, b) {
+    return a + b / 2;
+    }
+    // The function calculates the average of the two numbers by adding them together and dividing the sum by 2. 
+    console.log(average(2, 1)); // in this way the output will br 2.5
+
+    // But the correct way to calculate the correct average is by using () parentheses on operation of addition i.e.(a+b)/2  and output will be 1.5 which is correct
